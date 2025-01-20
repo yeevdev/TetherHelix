c8c4598061f86ad8a51c8063a06fa86249fa0018
ReenAG <heeboy007@naver.com>
2025-01-20 18:50:23 +0900

    env: better package management

--- tetherhelix_pyenv.sh
+++ tetherhelix_pyenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf tetherhelix_pyenv
-python3 -m venv tetherhelix_pyenv && source tetherhelix_pyenv/bin/activate && pip install pyupbit pandas numpy ta requests python-dotenv
+python3 -m venv tetherhelix_pyenv && source tetherhelix_pyenv/bin/activate && pip install -r requirements.txt
