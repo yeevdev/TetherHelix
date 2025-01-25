c98f47791a1ce252d63a3eb1dab8ed1883684263
ReenAG <heeboy007@naver.com>
2025-01-25 12:51:04 +0900

    fix: applied sqlite3 option for env

--- database/client/manager.py
+++ database/client/manager.py
@@ -5,12 +5,15 @@ from environments.variables import SQL_MODE
 
 from database.client.sql_client import SQLClient
 from database.client.implementation.mysql import MySQLClient
+from database.client.implementation.sqlite3 import SQLite3Client
 
 class SQLManager(metaclass=Singleton):
     def __init__(self, client_override=Optional[SQLClient]):
         print(f"SQLManager Mode : {SQL_MODE()}, override : {client_override}")
         if SQL_MODE() == "MySQL":
             self.client = MySQLClient()
+        elif SQL_MODE() == "SQLite3":
+            self.client = SQLite3Client()
         elif SQL_MODE() == "TEST" and client_override:
             print("SQLManager has detected test environment, overriding")
             self.client = client_override
