0190ea3e9ba4f328d6c41949579c5134319c0659
김건우 <starryeev@gmail.com>
2025-01-25 19:30:42 +0900

    feat: trading-bot 클래스 변경, PositionManager 클래스 추가, 기타 리팩토링(미완성)

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import asyncio
+import datetime
+import logging
+from zoneinfo import ZoneInfo
 
 import pyupbit
 
-import trading.bot
+import trading.bot_lagacy
 import trading.trade
 from environments.variables import UPBIT_ACCESS_KEY
 from environments.variables import UPBIT_SECRET_KEY
 
 
+TICKER = "KRW-USDT"
+BUY_QUANTITY = 5.0
+STEP = 1
+
+
+def get_logger():
+    my_logger = logging.getLogger()
+    my_logger.setLevel(logging.INFO)
+    my_logger.addHandler(logging.FileHandler("test.log"))
+
+    return logging.getLogger()
+
 def main():
-    bot = trading.bot.TradingBot(UPBIT_ACCESS_KEY, UPBIT_SECRET_KEY)
-    #upbit = pyupbit.Upbit(UPBIT_ACCESS_KEY, UPBIT_SECRET_KEY)
-    asyncio.run(bot.run())
-    #asyncio.run(trading.trade.buy(upbit, "KRW-USDT", 1503, 5))
+    # bot = trading.bot.TradingBot(UPBIT_ACCESS_KEY, UPBIT_SECRET_KEY)
+    # upbit = pyupbit.Upbit(UPBIT_ACCESS_KEY, UPBIT_SECRET_KEY)
+    # asyncio.run(bot.run())
+    # asyncio.run(trading.trade.buy(upbit, "KRW-USDT", 1503, 5))
+    pass
+
 
 if __name__ == '__main__':
     main()
--- trading/position.py
+++ trading/position.py
@@ -1,12 +1,70 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+from zoneinfo import ZoneInfo
+from main import get_logger
+
+
+@dataclass
 class Position:
+    order_uuid: str
+    entry_price: float
+    volume: float
+    created_at: str = ""
+    is_open: bool = True
+    target_price: float = 0.0
 
-    def __init__(self, entry_price, volume, created_at, order_uuid):
-        self.entry_price = entry_price
-        self.volume = volume
-        self.order_uuid = order_uuid
-        self.created_at = created_at
-        self.is_open = True
+
+# Singleton Class
+class PositionManager:
+    _instance = None
+
+    positions: List[Position] = []
+
+    @classmethod
+    def get_position_manager(cls):
+        if not cls._instance:
+            cls._instance = PositionManager()
+        return cls._instance
 
 
-    def close(self):
-        self.is_open = False
+    def create_position(self, order_uuid, entry_price, volume):
+        position = Position(order_uuid=order_uuid, entry_price=entry_price, volume=volume)
+        position.target_price = position.entry_price + 1  # 목표가격
+
+        time = datetime.now(ZoneInfo("Asia/Seoul"))
+        position.created_at = time.strftime("%Y-%m-%dT%H:%M:%S")
+
+        self.positions.append(position)
+
+
+    def remove_position(self, position):
+        try:
+            self.positions.remove(position)
+
+        except ValueError as e:
+            get_logger().error(f"포지션 삭제 중 오류 발생: {e}")
+
+
+    def get_position_by_uuid(self, order_uuid):
+        for position in self.positions:
+            if position.order_uuid == order_uuid:
+                return position
+
+        return None
+
+
+    def get_position_by_target_price(self, target_price):
+        for position in self.positions:
+            if position.target_price == target_price:
+                return position
+
+        return None
+
+
+    def is_positions_empty(self):
+        return True if not self.positions else False
+
+
+    def get_last_position(self):
+        return self.positions[-1] if self.positions[-1] else None
