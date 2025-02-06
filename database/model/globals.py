00f4e4aee13cfc28ee80374c118bfa1c7ba5a29a
ReenAG <heeboy007@naver.com>
2025-02-06 17:09:43 +0900

    feat: globals service implemented

--- database/model/globals.py
+++ database/model/globals.py
@@ -5,17 +5,12 @@ from typing import List, Optional
 from database.sqlite3 import SQLite3Client
 from database.model.dto.db_global_data import DBGlobalData
 
-from tetherhelix_grpc.transaction_pb2 import TransactionData
+from tetherhelix_grpc.globals_pb2 import GlobalStatusData
 
 from util.singleton import Singleton
-from util.timestamp import generate_isotimestamp
 from util.logger import Logger
 
-class OrderStatus:
-    BID_PLACED = 1
-    BID_FILLED = 2
-    ASK_PLACED = 3
-    ASK_FILLED = 4
+from trading.bot import TICKER
 
 class GlobalsManager(metaclass=Singleton):
     def __init__(self):
@@ -44,7 +39,7 @@ class GlobalsManager(metaclass=Singleton):
         """
         self.client.execute_with_commit(query)
 
-    def get_global_stats(self):
+    def get_global_stats(self) -> GlobalStatusData:
         #bot의 전체 상황을 볼때 쓰는 함수.
         query = """
         SELECT * FROM globals
@@ -52,4 +47,7 @@ class GlobalsManager(metaclass=Singleton):
         LIMIT 1;
         """
         result = self.client.execute_with_select(DBGlobalData, query)[0]
-        return result
+        current = pyupbit.get_current_price(TICKER)
+        rate = result.total_revenue / result.total_finished_transaction_count
+        data = GlobalStatusData(**result, bot_id="KRW-USDT", current_price=current, krw_gain_per_finished_transaction=rate)
+        return data
