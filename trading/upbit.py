6f360498a1b8772529804bb395d4e19ebd383866
ReenAG <heeboy007@naver.com>
2025-02-13 14:25:44 +0900

    fix: misleading error message for upbit wrong ip is fixed

--- trading/upbit.py
+++ trading/upbit.py
@@ -4,6 +4,7 @@ from environments.variables import UPBIT_ACCESS_KEY
 from environments.variables import UPBIT_SECRET_KEY
 
 from util.singleton import Singleton
+from util.logger import Logger
 
 class UpbitClientManager(metaclass=Singleton):
     upbit_client = pyupbit.Upbit(UPBIT_ACCESS_KEY, UPBIT_SECRET_KEY)
@@ -14,5 +15,8 @@ class UpbitClientManager(metaclass=Singleton):
     
     def currency(self):
         balances = self.upbit_client.get_balances()
+        if "error" in balances:
+            Logger.get_logger().error(balances);
+            raise Exception("upbit 에러. ip세팅 요망. 해당 코드 trading/upbit.py에 있음");
         current_krw = next(item for item in balances if item["currency"] == "KRW")["balance"]
         return float(current_krw)
